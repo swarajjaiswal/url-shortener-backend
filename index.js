905420d360068e6e216bc64bbdaa1735823854ee
swarajjaiswal <swaraj93310@gmail.com>
2025-01-02 23:27:45 +0530

    env updated

--- index.js
+++ index.js
@@ -4,41 +4,44 @@ const path = require('path');
 const urlRouter = require('./routes/url');
 const staticRoute = require('./routes/staticRouter');
 const userRouter = require('./routes/user');
-const cookieParser = require('cookie-parser')
-const { restrictLogin } = require('./middleware/auth')
+const cookieParser = require('cookie-parser');
+const { restrictLogin } = require('./middleware/auth');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
+const DB =process.env.MONGO_URL;
+
+// Connect to MongoDB
+mongoose.connect(DB)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.log('Not connected to MongoDB', err);
+});
+
 const app = express();
 
-app.use(cookieParser())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))  
-app.set("view engine", "ejs")
-app.set("views", path.resolve("./views"))
+// Middleware
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('./views'));
 
-const DB = process.env.MONGO_URL;
-//mongo connect
-mongoose.connect(DB)
-    .then(() => {
-        console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-        console.log("not connected to MongoDB", err);
-    });
-
+// Routes
 app.use('/url', restrictLogin, urlRouter);
-app.use('/', staticRoute)
-// app.get('/', (req, res) => {
-//     res.send('Home Page');
-// });
+app.use('/', staticRoute);
 app.use('/user', userRouter);
 
+// Example user page
 app.get('/userpage', (req, res) => {
-    res.send('User Page availabg');
-})
+    res.send('User Page available');
+});
+
+// Port setup
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log("Server Created");
+    console.log('Server created on port', PORT);
 });
